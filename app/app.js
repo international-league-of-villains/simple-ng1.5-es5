662257c307af1aaccb4ab676b08dcd579e69a34c
amindunited <buckley.robin@gmail.com>
2017-04-25 23:07:06 +1000

    progress commit

--- app/app.js
+++ app/app.js
@@ -1,16 +1,63 @@
 angular.module('oldNG1', []);
+
 angular.module('oldNG1')
-.directive('oldSelect', [function () {
-  return {
-    template: '<div>You have selected: {{currentSelection.name ? currentSelection.name : "nothing, yet..."}}</div><select ng-model="currentSelection" ng-options="option.name for option in options" placeholder="Please Choose"></select>',
-    controller: ['$scope', function ($scope) {
-      $scope.name = 'oldSelect'
-      $scope.currentSelection;
-      $scope.options = [{
-        name:'option 1'
-      }, {
-        name:'option 2'
+.directive('oldSelectWrapper', [function () {
+    return {
+      template: '<h3>ES5 Wrapping Component</h3><power-select first-names="$ctrl.firstNames" last-names="$ctrl.lastNames" callback="$ctrl.selection"></power-select>',
+      controllerAs: '$ctrl',
+      controller: [function () {
+        this.firstNames = [
+          { name: 'Doctor' },
+          { name: 'The Black' },
+          { name: 'The Red' },
+          { name: 'The Creeping' },
+          { name: 'Shadowy'},
+          { name: 'Grimm'}, 
+          { name: 'The Violent' },
+          { name: 'The Dark' },
+          { name: 'Unholy' },
+          { name: 'Terrible' },
+          { name: 'The Evil' }
+        ];
+        this.lastNames = [
+          { name: 'Black' },
+          { name: 'Hell' },
+          { name: 'Creep' },
+          { name: 'Terror' },
+          { name: 'Darkness' },
+          { name: 'Brood' },
+          { name: 'Shadow' },
+          { name: 'Evil' }
+        ];
+        this.selection = function (firstName, lastName) {
+          console.log('the callback method got', firstName, lastName);
+        }
       }]
+    }
+}])
+
+angular.module('oldNG1')
+.component('powerSelect', {
+    bindings: {
+      firstNames: '=',
+      lastNames: '=',
+      callback: '='
+    },
+    template: '<article>' +
+              '<h4>Select Villan Name (ES5)</h4>' +
+              '<select ng-model="$ctrl.firstName" ng-change="$ctrl.onSelect()" data-ng-options="firstName as firstName.name for firstName in $ctrl.firstNames">' +
+                '<option value="">Pick one...</option>' +
+              '</select>' +
+              '<select ng-model="$ctrl.lastName" ng-change="$ctrl.onSelect()" data-ng-options="lastName as lastName.name for lastName in $ctrl.lastNames">' +
+                '<option value="">Pick one...</option>' +
+              '</select>' +
+              '<span ng-if="$ctrl.firstName &&  $ctrl.lastName"> You selected selected the name: {{$ctrl.firstName.name}} {{$ctrl.lastName.name}}</span>' +
+            '</article>',
+    controller: ['$scope', function ($scope) {
+      this.onSelect = function () {
+        if (this.firstName && this.lastName) {
+          this.callback(this.firstName, this.lastName);
+        }
+      };
     }]
-  };
-}]);
+  });
